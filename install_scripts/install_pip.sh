afd9855c3d8b5361dab50365b1b5a2f6033dbc49
Tyler Lugger <notnottyler@gmail.com>
2020-02-23 19:05:27 +0000

    bypass y/n prompt with -y

--- install_scripts/install_pip.sh
+++ install_scripts/install_pip.sh
@@ -1,6 +1,6 @@
 echo "Installing Python 3 pip..."
 echo "--------------------------"
-sudo apt install python3-pip
+sudo apt install python3-pip -y
 
 echo "\n\nInstalling virtualenv package"
 echo "---------------------------------"
